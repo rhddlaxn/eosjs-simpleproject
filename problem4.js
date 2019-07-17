1b8a54804e88ac405535be178ae77d6034945f4b
rhddlaxn <rhddlaxn123@naver.com>
2019-07-17 10:22:08 +0900

    solve p4

--- problem4.js
+++ problem4.js
@@ -10,14 +10,29 @@ const signatureProvider = new JsSignatureProvider(privateKeys);
 const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
 
 async function main() {
-    try {
-        const result = await rpc.get_controlled_accounts(BP);
-        console.log(result);
-        
-            } catch (error) {
-        console.error(error);
-        
-    }
-
+let arr = [];
+try {
+const info = await rpc.get_info();
+const firstBP = info.head_block_producer;
+const firstBlockNumber = info.head_block_num;
+arr.push(info.head_block_producer);
+while(1) {
+const info = await rpc.get_info();
+const nextBP = info.head_block_producer;
+const nextBlockNumber = info.head_block_num;
+if(arr[0] === nextBP) {
+if(nextBlockNumber >= firstBlockNumber + 12)
+break;
+} else {
+if(arr.indexOf(nextBP) == -1)
+arr.push(nextBP);
 }
+}
+for(let i=0; i<arr.length; i++)
+console.log(arr[i]);
+} catch (error) {
+console.error(error);
+}
+}
+
 main();
